825e6c221dab1eed9868e219cf64e8481834f7c1
faucon <faucon.remi04@gmail.com>
2022-08-02 16:35:56 +0200

    sessions, body parser (post request), connection

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
 let express = require('express')
 let app = express()
+let bodyParser = require('body-parser')
+let session = require('express-session')
 let socket = require('socket.io')
+
 
 app.set('view engine', 'ejs')
 
-app.use('/assets', express.static('public'))
 
+// middlewares
+app.use('/assets', express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
+
+
+// routes
 app.get('/', (req, res) => {
     res.render('pages/index')
 })
+
+app.post('/', (req, res) => {
+    if (req.body.name === undefined || req.body.name.trim() === ""){
+        req.session.name = "annonyme"
+    }
+    else {
+        req.session.name = req.body.name
+    }
+    res.redirect("/home")
+})
+
 
 app.listen(3000)
