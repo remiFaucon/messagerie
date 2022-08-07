e2118411bb7769db065e0c514f4b5a0b9739a089
faucon <faucon.remi04@gmail.com>
2022-08-07 18:02:40 +0200

    peerjs backup

--- public/script/visio.js
+++ public/script/visio.js
@@ -1,10 +1,22 @@
 let user = { id: undefined }
 socket.on('thisIsYourId', (myId) => {
     document.querySelector(".headChat img").addEventListener('click', () => {
-        const peer = new Peer(Math.round(Math.random()*100000), {
+        const peer = new Peer(Math.round(Math.random()*1000000000), {
             host: "192.168.1.120",
             port: 3000,
-            path: "/peerjs/visio"
+            path: "/peerjs/visio",
+            secure: true,
+            // config: {
+            //     'iceServers': [
+            //         { url: 'stun:stun1.l.google.com:19302' },
+            //         {
+            //             url: 'turn:numb.viagenie.ca',
+            //             credential: 'muazkh',
+            //             username: 'webrtc@live.com'
+            //         }
+            //     ]
+            //     },
+            debug: 2
         })
 
         let room = document.querySelector("h2").getAttribute('data-room-id')
@@ -23,35 +35,39 @@ socket.on('thisIsYourId', (myId) => {
 
                 socket.on('newUserVisio', (userId) => {
                     const call = peer.call(userId[0], mediaStream)
-                    call.on("stream", (stream) => {
-                        newUserVisio(stream, "received").then(() => console.log("cest good")).catch(() => console.log("eh bas non"))
-                    })
-                    call.on("data", (stream) => {
-                        document.getElementById("received").srcObject = stream
-                    })
-                })
+                    console.table(call)
 
-                peer.on("call", (call) => {
-                    call.answer(mediaStream)
                     call.on("stream", (stream) => {
                         newUserVisio(stream, "received")
                     })
-                    call.on("data", (stream) => {
-                        document.getElementById("received").srcObject = stream
-                    })
-                })
+                    // call.on("data", (stream) => {
+                    //     document.getElementById("received").srcObject = stream
+                    // })
 
-                // call.on("error", (err) => {
-                //     console.log(err)
-                // })
-                // call.on('close', () => {
-                //     // endCall()
-                // })
+
+                    peer.on("call", (call) => {
+                        call.answer(mediaStream)
+                        call.on("stream", (stream) => {
+                            newUserVisio(stream, "received")
+                        })
+                        // call.on("data", (stream) => {
+                        //     document.getElementById("received").srcObject = stream
+                        // })
+                        call.on('close', () => {
+                            // endCall()
+                            console.log("ca a couper")
+                        })
+                    })
+
+                    peer.on("error", (err) => {
+                        console.log(err)
+                    })
+
+                })
             })
     })
 })
-
-const newUserVisio = async (stream, idVideo) => {
+const newUserVisio = (stream, idVideo) => {
     let userCam = document.createElement("video")
     userCam.id = idVideo
     userCam.srcObject = stream
@@ -59,11 +75,11 @@ const newUserVisio = async (stream, idVideo) => {
         userCam.play()
     })
 
-    stream.addEventListener('error', event => {
-        const error = event.path[0].error;
-        console.log(error);
-        alert(error.message);
-    }, true);
+    // stream.addEventListener('error', event => {
+    //     const error = event.path[0].error;
+    //     console.log(error);
+    //     alert(error.message);
+    // }, true);
 
     let visioDiv = document.querySelector('.visio')
     userCam.setAttribute("autoplay", "autoplay")
